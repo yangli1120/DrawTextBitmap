e8e885526865e8b34cbac7d18b15ec5f916d40d7
Young Lee <ly513782705@gmail.com>
2014-07-30 17:14:24 +0800

    init this demo
    Signed-off-by: Young Lee <ly513782705@gmail.com>

---
+++ src/com/example/drawtextbitmap/DrawTextBitmapUtils.java
@@ -0,0 +1,60 @@
+package com.example.drawtextbitmap;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Typeface;
+import android.text.Layout.Alignment;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+
+public class DrawTextBitmapUtils {
+
+    public static void drawVoiceBitmap(Bitmap bg, String msg) {
+        Canvas canvas = new Canvas(bg);
+
+        TextPaint mStrokePaint = new TextPaint();
+        mStrokePaint.setTextSize(40f);
+        mStrokePaint.setColor(0xff000000);
+        mStrokePaint.setAntiAlias(true);
+
+        mStrokePaint.setStyle(Paint.Style.FILL);
+        //mStrokePaint.setStrikeThruText(true);
+        mStrokePaint.setHinting(Paint.HINTING_OFF);
+        //mStrokePaint.setShadowLayer(10f, -1f, -1f, 0xffffffff);
+        mStrokePaint.setSubpixelText(true);
+        mStrokePaint.setFilterBitmap(false);
+        mStrokePaint.setTypeface(Typeface.DEFAULT_BOLD);
+
+        /*mStrokePaint.setStrokeCap(Paint.Cap.BUTT);
+        mStrokePaint.setStrokeJoin(Paint.Join.MITER);
+        mStrokePaint.setStrokeMiter(10f);
+        mStrokePaint.setStrokeWidth(10f);*/
+        canvas.save();
+
+        StaticLayout strokeLayout = new StaticLayout(msg, mStrokePaint, bg.getWidth() * 3 / 5,
+                Alignment.ALIGN_CENTER, 1.0f, 1.0f, false);
+
+        canvas.translate(bg.getWidth() / 5, bg.getHeight() - strokeLayout.getHeight() - 20);
+
+        TextPaint mTextPaint = new TextPaint();
+        mTextPaint.setTextSize(40f);
+        mTextPaint.setColor(0xffffffff);
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setStyle(Paint.Style.STROKE);
+        mTextPaint.setStrokeWidth(10f);
+        mTextPaint.setShadowLayer(1f, 0, 0, Color.WHITE);
+        mTextPaint.setFakeBoldText(true);
+        mTextPaint.setFilterBitmap(false);
+        mTextPaint.setTypeface(Typeface.DEFAULT_BOLD);
+        StaticLayout textLayout = new StaticLayout(msg, mTextPaint, bg.getWidth() * 3 / 5,
+                Alignment.ALIGN_CENTER, 1.0f, 1.0f, false);
+
+        textLayout.draw(canvas);
+        strokeLayout.draw(canvas);
+        canvas.restore();
+    }
+
+}
---
+++ src/com/example/drawtextbitmap/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.drawtextbitmap;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private ImageView mPreviewIv;
+    private EditText mInputEt;
+    private TextView mDrawTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPreviewIv = (ImageView)findViewById(R.id.preview_iv);
+        mInputEt = (EditText)findViewById(R.id.input_et);
+        mDrawTv = (TextView)findViewById(R.id.draw_tv);
+        mDrawTv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.draw_tv: {
+                if(!TextUtils.isEmpty(mInputEt.getEditableText().toString())) {
+                    Bitmap bm = BitmapFactory.decodeResource(getResources(),
+                            R.drawable.bitch).copy(Bitmap.Config.ARGB_8888, true);
+                    DrawTextBitmapUtils.drawVoiceBitmap(bm,
+                            mInputEt.getEditableText().toString());
+                    mPreviewIv.setImageBitmap(bm);
+                }
+            }break;
+        }
+    }
+
+}
